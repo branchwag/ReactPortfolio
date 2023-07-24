ee8792f41ccbe98f082644756338c096357a679e
LeAnne B <branchleannelmb@gmail.com>
2023-07-24 17:19:05 -0400

    fixes resume section link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,16 +13,25 @@ export default function Navbar() {
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#about" className="mr-5 hover:text-white">
+          <a href="#about" className="mr-5 hover:text-white active:violet-700">
             About Me
           </a>
-          <a href="#projects" className="mr-5 hover:text-white">
+          <a
+            href="#projects"
+            className="mr-5 hover:text-white"
+          >
             Portfolio
           </a>
-          <a href="#contact" className="mr-5 hover:text-white">
+          <a
+            href="#contact"
+            className="mr-5 hover:text-white"
+          >
             Contact
           </a>
-          <a href="#testimonials" className="mr-5 hover:text-white">
+          <a
+            href="#resume"
+            className="mr-5 hover:text-white"
+          >
             Resume
           </a>
         </nav>
