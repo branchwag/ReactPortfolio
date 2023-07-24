2399a4e802e9723a5fc4d4e0f91b36ef299b3c21
LeAnne B <branchleannelmb@gmail.com>
2023-07-24 15:40:47 -0400

    adds in rounded pic styling using tailwind

--- src/components/About.js
+++ src/components/About.js
@@ -34,7 +34,7 @@ export default function About() {
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
-            className="object-cover object-center rounded"
+            className="object-cover object-center rounded-full w-96 h-96"
             alt="hero"
             src="./profilepic2.jpg"
           />
