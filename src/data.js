2f4c603d8027e380acbe1069798bfe42763b0dfc
LeAnne B <branchleannelmb@gmail.com>
2023-07-24 16:52:32 -0400

    adds in projects

--- src/data.js
+++ src/data.js
@@ -1,35 +1,48 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "LegendSmith",
+    subtitle: "A character creation app",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+      "Powered by Chatgpt, this app generates a character name and backstory",
+    image: "./chargenscreenshot.png",
+    link: "https://legendsmith-77384eb64fff.herokuapp.com/login",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    title: "The Breakroom",
+    subtitle: "An Office-themed arcade",
+    description: "A fun arcade of office-themed arcade games",
+    image: "./breakroomscreenshot.png",
+    link: "https://sefenydiaz.github.io/test-arcade/index.html",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "CSS Snippets",
+    subtitle: "A CSS Study Page",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+      "A helpful guide for devs who need a refresher on styling tricks",
+    image: "./csssnippetpage.png",
+    link: "https://branchwag.github.io/Module2-CSSPage/",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "ChatGPT Page",
+    subtitle: "A landing page to use ChatGPT",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+      "A simple page to chat with your favorite text-generating AI friend",
+    image: "./chatgptpage.png",
+    link: "https://github.com/branchwag/PortfolioWebpage",
+  },
+  {
+    title: "Not Another Text Editor",
+    subtitle: "An interactive web-based text editor",
+    description: "A single-page PWA for jotting down a few notes",
+    image: "./fundragonscreenshot.png",
+    link: "https://pwatexteditorlmb-5ee75f4be028.herokuapp.com/",
+  },
+  {
+    title: "SVG Logo Generator",
+    subtitle: "An CLI for generating SVG Logos",
+    description: "A command-line interface for generating simple shape logos",
+    image: "./logogenscreen.png",
+    link: "https://github.com/branchwag/SVG-Logo-Maker",
   },
 ];
 
