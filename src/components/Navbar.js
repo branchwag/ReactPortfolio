8908c3f510519adb4fc621496c78378470cc18fd
LeAnne B <branchleannelmb@gmail.com>
2023-07-25 09:42:39 -0400

    fixes resume block

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <header className="bg-purple-900 md:sticky top-0 z-10">
+    <header className="bg-purple-900 md:sticky top-0 z-20">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
 
 export default function Resume() {
   return (
-    <section id="resume">
+    <section className="z-0" id="resume">
       <div className="container px-5 py-10 mx-auto text-center">
         <UsersIcon className="w-10 inline-block mb-4" />
         <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
